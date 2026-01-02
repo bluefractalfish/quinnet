66f27de530ae8403edad93b234e8d5a301cf9c3f
bluefractalfish <blue@fractal.fish>
2026-01-01 21:52:58 -0700

    none

---
+++ scripts/grid.js
@@ -0,0 +1,39 @@
+const grid = document.getElementById("grid");
+const CELL_SIZE=40;
+STRENGTH=100;
+
+function buildGrid(){
+  const cols = Math.ceil(window.innerWidth/CELL_SIZE);
+  const rows = Math.ceil(window.innerHeight/CELL_SIZE);
+  grid.innerchar="";
+  for (let i = 0; i<cols*rows;i++){
+    const span = document.createElement("span");
+    span.textContent="+";
+    grid.appendChild(span);
+  }
+}
+
+buildGrid();
+window.addEventListener("resize",buildGrid);
+window.addEventListener("mousemove",(e)=>{
+  const spans = grid.children;
+  for (const span of spans){
+
+  const rect = span.getBoundingClientRect();
+  const cx = rect.left + rect.width / 2;
+  const cy = rect.top + rect.height / 2;
+
+  const dx = e.clientX - cx;
+  const dy = e.clientY - cy;
+
+  const dist = Math.hypot(dx, dy);
+  const strength = Math.max(0, 1 - dist / STRENGTH);
+  span.style.fontWeight = Math.round(strength*(800))
+  span.style.transform = `
+    translate(
+      ${dx * strength * 0.1}px,
+      ${dy * strength * 0.1}px
+    )
+  `;
+  }
+});
